f312a67eee9787cfbfd290d1941aa1a4a0536e32
bricar25 <130022676+bricar25@users.noreply.github.com>
2024-07-16 01:46:26 -0700

    Add files via upload
    The web scrapper was built using BeautifulSoup4. So far, it extracts the title, URL, and the entire text of articles on policyandpoliticsblog.com

---
+++ test.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Tells the program which website to scrape.
+URL = "https://policyandpoliticsblog.com"
+
+# Store response to GET request
+page = requests.get(URL)
+
+# 200 request indicates scrape was successful
+# Checks if successful request
+if page.status_code == 200:
+    # If successful request, BeautifulSoup object created from scrape 
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Finds the <article> tag
+    post = soup.find('article')
+
+    if post:
+        # Finds the title of the article, then extracts the title content.
+        title = post.find('h2').get_text()
+
+        # Finds the URL of the article, then extracts the URL content.
+        article_url = post.find('a')['href']
+
+        # Finds the text in the article, then extracts the entire article.
+        text = post.find_all('p')
+
+        print(title)
+        print(article_url)
+        print(text)
